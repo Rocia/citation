ede1a7e4af2e3e52eae46fa9897613ccf4fa6dac
ROCIA FERNANDES <rocia.fernandes@gmail.com>
2017-08-17 17:19:16 +0530

    modals added

---
+++ brython1.php
@@ -0,0 +1,41 @@
+<?php
+
+$allowedExts = array("xml");
+$extension = end(explode(".", $_FILES["file"]["name"]));
+if ((($_FILES["file"]["type"] == "text/xml"))
+&& ($_FILES["file"]["size"] < 5000000)
+&& in_array($extension, $allowedExts))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+    if (file_exists("xml/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "xml/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "xml/" . $_FILES["file"]["name"];
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
+
+
+
+
+
+?>
